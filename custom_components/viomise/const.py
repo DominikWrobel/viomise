d12d3a021d2d76c0cd592674eb81668cbee53c3a
Dominik <89667597+DominikWrobel@users.noreply.github.com>
2024-10-03 15:35:40 +0200

    Update const.py
    v1.2

--- custom_components/viomise/const.py
+++ custom_components/viomise/const.py
@@ -1,3 +1,4 @@
 """Constants for the Xiaomi Vacuum integration."""
-DOMAIN = "viomise"
+DOMAIN = "viomise"  # This should be consistent across all files
+DATA_KEY = f"{DOMAIN}.device"  # Update this from the previous "vacuum.miio2"
 DEFAULT_NAME = "Viomi SE"
