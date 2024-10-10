f4e0d13a74672a607471182d90d3636c6ef33ac6
Dominik <89667597+DominikWrobel@users.noreply.github.com>
2024-10-10 18:50:38 +0200

    Update __init__.py
    v1.4

--- custom_components/viomise/__init__.py
+++ custom_components/viomise/__init__.py
@@ -1,15 +1,16 @@
 """The Xiaomi Vacuum integration."""
-from __future__ import annotations
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import Platform
 from homeassistant.core import HomeAssistant
+
 from .const import DOMAIN
 
-PLATFORMS: list[Platform] = [Platform.VACUUM]
+PLATFORMS: list[Platform] = [Platform.VACUUM, Platform.SENSOR]
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up Xiaomi Vacuum from a config entry."""
     hass.data.setdefault(DOMAIN, {})
+    hass.data[DOMAIN][entry.entry_id] = {}
     
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
     return True
@@ -19,5 +20,3 @@ async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     if unload_ok := await hass.config_entries.async_unload_platforms(entry, PLATFORMS):
         hass.data[DOMAIN].pop(entry.entry_id)
     return unload_ok
-
-
