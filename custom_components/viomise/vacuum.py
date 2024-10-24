0e064c11726704b03deea13fae539f83c9894031
Dominik <89667597+DominikWrobel@users.noreply.github.com>
2024-10-25 01:16:06 +0200

    Update vacuum.py

--- custom_components/viomise/vacuum.py
+++ custom_components/viomise/vacuum.py
@@ -296,7 +296,7 @@ class MiroboVacuum2(StateVacuumEntity):
     def battery_level(self):
         """Return the battery level of the vacuum cleaner."""
         if self.vacuum_state is not None:
-            return self.vacuum_state['battary_life']
+            return self.vacuum_state['battery_life']
 
     @property
     def fan_speed(self):
